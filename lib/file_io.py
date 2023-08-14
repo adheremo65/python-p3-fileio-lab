b968c9a862664f897063718b95e770796ff49eb8
Abdela Umer <adheremo65@gmail.com>
2023-08-14 01:22:05 -0400

    file write read lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+   path = str(file_name) + ".txt"
+   file =  open(path, mode="w" ,encoding="utf-8")
+   file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+     path = str(file_name) + ".txt"
+     file = open(path,mode="a",encoding="utf-8")
+     file.write(append_content)
+    
 
 def read_file(file_name):
+    path = str(file_name) + ".txt"
+    file = open(path, mode="r", encoding="utf-8")
+    return file.read()
     pass
